e5027575e1254837395cefd796fe257b0e41432f
Ali Mahmoud <44996407+AliiMahmoud@users.noreply.github.com>
2020-05-02 02:46:47 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,14 @@
-// database connection
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'remotemysql.com',
+    port     : '3306',
+    user     : 'TBWURxMMlC',
+    password : 'mDaLb01xcw',
+    database : 'TBWURxMMlC'
+});
+connection.connect(function(err) 
+{
+    if (err) throw err;
+});
+
+module.exports = connection;
