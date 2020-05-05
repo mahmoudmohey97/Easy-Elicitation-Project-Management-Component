d6c7e4aef470d853208e75f111bb755ccc2ba317
mahmoudmohey97 <mahmoudmoheyeldin97@gmail.com>
2020-05-05 21:58:08 +0200

    new skeleton of project

---
+++ models/project.js
@@ -0,0 +1,11 @@
+const conn = require('../database');
+/*var express = require('express');
+var path = require('path');
+var app = express();
+*/
+module.exports.login = function () 
+{
+   
+};
+
+
